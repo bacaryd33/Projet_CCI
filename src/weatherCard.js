269ab029f481a587bf04a31cadd1fb9e00eb105b
quentin aubry <quentin>
2018-12-07 01:41:32 +0100

    recup de la ville json

--- src/weatherCard.js
+++ src/weatherCard.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class WeatherCard extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      name:"",
+
+    }
+  }
+  componentDidMount() {
+    const url = 'https://openweathermap.org/data/2.5/weather?lat=44.52&lon=3.5&appid=b6907d289e10d714a6e88b30761fae22';
+    axios.get(url)
+      .then(response => {
+        console.log(response.data.name);
+        this.setState({
+          name: response.data.name,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+
+
+
   render() {
     return (
             <div className="card mb">
                 <p className="card-text" className="text-center">
-                      LOZERE
+                      {this.state.name}
                 </p>
                 <img className="card-img-top" src="https://www.edupics.com/image-01-partly-cloudy-dm9922.jpg" alt="Card image cap"/>
                 <div className="card-body mb">
